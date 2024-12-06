422365067274430f246dc736a6f98dfa445621d5
asishsahu946 <asishsahu946@gmail.com>
2024-12-06 13:25:17 +0530

    bug fixed

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ This is a Streaming plartform with latest movies update with trailer and details
 ## Screenshots
 
 ![Home Page - Laptop](https://github.com/user-attachments/assets/4263fae3-d3fa-47f2-8ab9-831bc3b5aa48)
-![Movies   Shows Page - Laptop](https://github.com/user-attachments/assets/52d9899e-533e-4e3f-8ca2-b3122c20ccb6)
-![Movies Page Open - Laptop](https://github.com/user-attachments/assets/e17fb044-eb60-4061-ac02-94e8ef05f6d7)
-![Support Page - Laptop](https://github.com/user-attachments/assets/15dcba55-1eb5-4c20-a764-adb1b9614236)
-![Subscription Page - Laptop](https://github.com/user-attachments/assets/fb6952ad-24d6-42bd-b4c0-fac16b97c619)
 
 ## Website Template Used
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,55 +4,54 @@ module.exports = withMT({
   theme: {
     extend: {
       fontFamily: {
-       'sans': ['"Manrope"'],
+        sans: ['"Manrope"'],
       },
       backgroundColor: {
-        'custom-bg': '#141414', // Example custom color
-      }
+        "custom-bg": "#141414", // Example custom color
+      },
     },
     screens: {
-      'desktop' : {'max': '1920px'},
-      'laptop' : {'max': '1440px'},
-      'mobile' : {'max': '390px'},
-      '2xl-max': {'max': '1535px'}, // => @media (max-width: 1535px) { ... }
-      'xl-max': {'max': '1279px'}, // => @media (max-width: 1279px) { ... }
-      'lg-max': {'max': '1023px'}, // => @media (max-width: 1023px) { ... }
-      'md-max': {'max': '767px'}, // => @media (max-width: 767px) { ... }
-      'sm-max': {'max': '639px'}, // => @media (max-width: 639px) { ... }
-      '2xl': '1535px', 
-      'xl': '1279px', 
-      'lg': '1023px', 
-      'md': '767px', 
-      'sm': '639px', 
+      desktop: { max: "1920px" },
+      laptop: { max: "1440px" },
+      mobile: { max: "390px" },
+      "2xl-max": { max: "1535px" }, // => @media (max-width: 1535px) { ... }
+      "xl-max": { max: "1279px" }, // => @media (max-width: 1279px) { ... }
+      "lg-max": { max: "1023px" }, // => @media (max-width: 1023px) { ... }
+      "md-max": { max: "767px" }, // => @media (max-width: 767px) { ... }
+      "sm-max": { max: "639px" }, // => @media (max-width: 639px) { ... }
+      "2xl": "1535px",
+      xl: "1279px",
+      lg: "1023px",
+      md: "767px",
+      sm: "639px",
     },
     colors: {
-      'white' : '#fff',
-      'black' : '#000',
-      'red1' : '#e50000',
-      'red2' : '#ff0000',
-      'red3' : '#ff1919',
-      'red4' : '#ff3333',
-      'red5' : '#ff9999',
-      'red6' : '#ffcccc',
-      'red7' : '#ffe5e5',
-      'red8' : '#fffafa',
-      'black1' : '#0f0f0f',
-      'black2' : '#141414',
-      'black3' : '#1a1a1a',
-      'black4' : '#1f1f1f',
-      'black5' : '#262626',
-      'black6' : '#333333',
-      'black7' : '#404040',
-      'gray1' : '#999999',
-      'gray2' : '#a6a6a6',
-      'gray3' : '#b3b3b3',
-      'gray4' : '#bfbfbf',
-      'gray5' : '#e4e4e4',
-      'gray6' : '#f1f1f3',
-      'gray7' : '#f7f7f8',
-      'gray8' : '#fcfcfd',
-      
-    }
+      white: "#fff",
+      black: "#000",
+      red1: "#e50000",
+      red2: "#ff0000",
+      red3: "#ff1919",
+      red4: "#ff3333",
+      red5: "#ff9999",
+      red6: "#ffcccc",
+      red7: "#ffe5e5",
+      red8: "#fffafa",
+      black1: "#0f0f0f",
+      black2: "#141414",
+      black3: "#1a1a1a",
+      black4: "#1f1f1f",
+      black5: "#262626",
+      black6: "#333333",
+      black7: "#404040",
+      gray1: "#999999",
+      gray2: "#a6a6a6",
+      gray3: "#b3b3b3",
+      gray4: "#bfbfbf",
+      gray5: "#e4e4e4",
+      gray6: "#f1f1f3",
+      gray7: "#f7f7f8",
+      gray8: "#fcfcfd",
+    },
   },
   plugins: [],
 });
