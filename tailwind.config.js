adf9f0a2b34757c69b4548c73a4aa2422b061ba9
asishsahu946 <asishsahu946@gmail.com>
2024-12-06 12:59:22 +0530

    bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import { withMT } from "@material-tailwind/react/utils/withMT";
+const withMT = require("@material-tailwind/react/utils/withMT");
 module.exports = withMT({
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
